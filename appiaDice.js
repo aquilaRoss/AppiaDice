a8d8ca6fe1095748b4cbaa2e0b05a625f1f14c97
aquilaRoss <66177679+aquilaRoss@users.noreply.github.com>
2022-10-21 16:32:22 +0100

    Add files via upload

--- appiaDice.js
+++ appiaDice.js
@@ -8,12 +8,36 @@ function randomIntFromInterval(min, max)
   console.log("Random Number in Range " + min + " to " + max + " of " + this.randomInRange);
 }
 
+function getOdds(targetNumber)
+{
+  if (targetNumber <= 10)
+  {
+	return (11 - targetNumber) / 10;
+  }
+  else
+  {
+	return getOdds(10) * getOdds(targetNumber - 10);
+  }
+}
+
 function getData()
 {
-  this.dice = document.getElementById("numberOfDice").value;
-  this.tn = document.getElementById("targetNumber").value;
-  this.as = document.getElementById("automaticSucessess").value;
-  this.desired = document.getElementById("desiredResult").value;
+  this.dice = parseInt(document.getElementById("numberOfDice").value, 10);
+  this.tn = parseInt(document.getElementById("targetNumber").value, 10);
+  this.as = parseInt(document.getElementById("automaticSucessess").value, 10);
+  this.desired = parseInt(document.getElementById("desiredResult").value, 10);
+  
+  this.odds = getOdds(this.tn);
+  console.log("TN " + this.tn + " produces odds of " + this.odds);
+  
+  this.averageResultJustDice = this.dice * this.odds;
+  console.log("From the Dice, this gives you " + this.averageResultJustDice);
+  
+  this.averageResultRaw = this.averageResultJustDice + this.as;
+  console.log("Adding the AS makes it " + this.averageResultRaw);
+  
+  this.averageResult = Number(this.averageResultRaw.toFixed(2));
+  console.log("Rounding makes it " + this.averageResult);
   
   this.rollText = this.dice + "D10 TN" + this.tn + " ";
   if (this.as < 0) { this.rollText += " " + this.as + "AS"; }
@@ -25,6 +49,6 @@ function populateResults()
   var rawData = new getData();
   
   document.getElementById("diceRollded").innerHTML = rawData.rollText;
-  document.getElementById("averageResult").innerHTML = "3.7";
-  document.getElementById("percentageChance").innerHTML = "15";
+  document.getElementById("averageResult").innerHTML = rawData.averageResult;
+  document.getElementById("percentageChance").innerHTML = "NYI";
 }
